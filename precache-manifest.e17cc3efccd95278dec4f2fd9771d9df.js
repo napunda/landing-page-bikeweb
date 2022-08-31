c3a397f1873e7729fafe26569bd8ad5645880707
napund4 <napunda@mail.com>
2022-08-31 13:39:44 -0300

    build

---
+++ precache-manifest.e17cc3efccd95278dec4f2fd9771d9df.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f26fa982ac16f11557",
+    "url": "/css/app.ea56e42f.css"
+  },
+  {
+    "revision": "5dcd92462ac6a5cd1c50",
+    "url": "/css/chunk-vendors.10a3936c.css"
+  },
+  {
+    "revision": "8bc949191359d5634b4121ea2d816591",
+    "url": "/img/bg-banner.svg"
+  },
+  {
+    "revision": "f91365c7b92c316036ec09757240fd0a",
+    "url": "/img/bg-hero.png"
+  },
+  {
+    "revision": "881e7883a3d1ce19aa3846d8011a72f2",
+    "url": "/img/bikes-top.png"
+  },
+  {
+    "revision": "c6db48b741e74f136528702eebc83a2e",
+    "url": "/img/bikeweb.svg"
+  },
+  {
+    "revision": "2aca994557eadba91ef0f1618fd4c44f",
+    "url": "/img/caloi.png"
+  },
+  {
+    "revision": "8ba212cccbe214fe6443514b0604850c",
+    "url": "/img/colli.png"
+  },
+  {
+    "revision": "2c19696b2c8c1190c0ae8184d85b9596",
+    "url": "/img/ksw.png"
+  },
+  {
+    "revision": "64caff1a1de6d838d29f3660f4d5d14b",
+    "url": "/img/logo-about.svg"
+  },
+  {
+    "revision": "4820274e3a429868651972885b86a0ad",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "13b9c122ac76a1b4d29cd5b0fec83ee2",
+    "url": "/img/portfolio/bike-beach.jpg"
+  },
+  {
+    "revision": "922f4e5011a13f696f53ba0b41e7e1a7",
+    "url": "/img/portfolio/bike-downhill.png"
+  },
+  {
+    "revision": "7dbeb531cada39b787b4f067bc60a908",
+    "url": "/img/portfolio/bike-helmet.jpg"
+  },
+  {
+    "revision": "39a45cbd29870ba7a2c9c743a4d6e4cb",
+    "url": "/img/portfolio/bike-road.jpg"
+  },
+  {
+    "revision": "213b3d00b2600df5f8c6f7281995bbf5",
+    "url": "/img/portfolio/bike-roadway.jpg"
+  },
+  {
+    "revision": "a4075dd8565c4532b6b75d53b6111f3d",
+    "url": "/img/portfolio/bike-wheel.jpg"
+  },
+  {
+    "revision": "04fc361cd67c4cc2657ea48e3a79602a",
+    "url": "/img/rino.png"
+  },
+  {
+    "revision": "a3b41cc72c992a86ae71e0fdb327437a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f26fa982ac16f11557",
+    "url": "/js/app.e36e0a99.js"
+  },
+  {
+    "revision": "5dcd92462ac6a5cd1c50",
+    "url": "/js/chunk-vendors.ebb26e8b.js"
+  },
+  {
+    "revision": "0041c35e27fbba2873def1826c95a4f6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
